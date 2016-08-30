c3321e67beca5aafd5626a4560421ef20099fe1e
ThoLH <lhtho.ls@gmail.com>
2016-08-30 21:20:00 +0700

    - delete sync text tool

--- app/sync_text.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-java -jar TextSyncFromSpreadsheetTool.jar app-default.config
-java -jar TextSyncFromSpreadsheetTool.jar app-en.config
-java -jar TextSyncFromSpreadsheetTool.jar app-my.config
